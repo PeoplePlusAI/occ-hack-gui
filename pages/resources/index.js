3f9a9f90b8238643fd97f13aebba7c6b040bc7ab
Arpit Sureka <surekaarpit@gmail.com>
2024-10-05 19:34:35 +0530

    loader added

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Stack, Heading, Text, SimpleGrid, Divider, Link, Button, SlideFade, HStack } from '@chakra-ui/react'
-// import {ResourceCard, ResourceTypes, ResourceCardProps} from '../../components/ResourcePage/resourceCard'
+import { Stack, Heading, Text, SimpleGrid, Divider, Link, Button, SlideFade, HStack, Center } from '@chakra-ui/react'
 import {ResourceCard, ResourceTypes, ResourceCardProps} from '../../components/ResourcePage/resouceCardModal'
 import HorizontalFilterUI from '../../components/ResourcePage/filter'
 import Container from '../../components/Container'
@@ -12,6 +11,7 @@ import { BsGpuCard } from "react-icons/bs";
 import {fetchDataFromAPI} from '../api/resourceApi'
 import customTheme from '../../styles/theme'
 import {getProvidersList, getUseCasesList, getMaximumStorage, applyFilter} from '../../utils/resourcePage'
+import { Spinner } from '@chakra-ui/react'
 
 export default function Resources({ projects }) {
 
@@ -22,38 +22,30 @@ export default function Resources({ projects }) {
       "computeSelection" : null
     });
     const [ResourceCardList, setData] = useState([]);
-    // const [ResourceCardListFilter, setFilter] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [usecasesDropdown, setUsecasesDropdown] = useState([]);
     const [providersDropdown, setProviderssDropdown] = useState([]);
     const [maxStroage, setMaxStroage] = useState(100);
-    // const []
 
     useEffect(() => {
       const fetchData = async () => {
         try {
           const result = await fetchDataFromAPI();  // Call the API function
           setData(result);  // Update state with the fetched data
-          // setFilter(result);
           setUsecasesDropdown(getUseCasesList(result))
           setProviderssDropdown(getProvidersList(result))
           setMaxStroage(getMaximumStorage(result))
+          setLoading(false);
         } catch (error) {
           setError(error.message);  // Set error if any occurs
-        } finally {
-          setLoading(false);
-          // setLoading(false);  // Stop the loading state
         }
       };
 
       fetchData();  // Invoke the fetch function when the component mounts
     }, []);  // Empty array ensures this runs only once on mount
 
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-
+  
   return (
     <>
       <Container>
@@ -67,34 +59,18 @@ export default function Resources({ projects }) {
             <Heading color="displayColor" fontSize={{ base: '4xl', md: '6xl' }}>
               Resources
             </Heading>
-            {/* <Text fontSize={{ base: '14px', md: '16px' }}>
-              Select CPU, Storage or GPU
-            </Text> */}
-            {/* <SlideFade
-        direction="top"
-        transition={{ enter: { duration: 0.4, delay: 1.0 } }}
-        in={true}
-      >
-        <Stack isInline spacing={4}>
-        <SelectionIconButton text='GPU' icon={BsGpuCard} onClick={() => setSelection(ResourceTypes.GPU)} isActive={ResourceTypes.GPU==currentSelection} />
-        <SelectionIconButton text='Storage' icon={FaDatabase} onClick={() => setSelection(ResourceTypes.STORAGE)} isActive={ResourceTypes.STORAGE==currentSelection}/>
-        <SelectionIconButton text='Task' icon={FaServer} onClick={() => setSelection(ResourceTypes.TASK)} isActive={ResourceTypes.TASK==currentSelection}/>
-        </Stack>
-      </SlideFade> */}
       <HorizontalFilterUI theme={customTheme} providers={providersDropdown} usecases={usecasesDropdown} setFilterSelection={setFilters} filterData={currentFilters} maxStorage={maxStroage}/>
-            <Divider />
+            <Divider thickness="100px"/>
           </Stack>
           <HStack>
-            {/* <FilterUI /> */}
-          <SimpleGrid columns={{ sm: 2, md: 4 }} spacing={6}>
+            {loading ? <Center><Spinner pos='fixed' top='50%' left="50%" size='xl' thickness='6px'/></Center> : <SimpleGrid columns={{ sm: 2, md: 4 }} spacing={6}>
             {ResourceCardList.map(function(object, i){
               if(applyFilter(object, currentFilters)){
                 return <ResourceCard key={i} resourceCard={object} />;
               }
-            })}
-            {/* {ResourceCardList.map((object, i) => <ResourceCard key={i} resourceCard={object}/>)} */}
-            
-          </SimpleGrid>
+            })}            
+          </SimpleGrid>}
+          
           </HStack>
           
         </Stack>
