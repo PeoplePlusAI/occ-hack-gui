1d18a2e82cd24c90d6c8b1b2a3d672c0f53e8d47
Arpit Sureka <surekaarpit@gmail.com>
2024-10-17 13:31:05 +0530

    chakra ui code clean

--- components/ResourcePage/selectionButton.tsx
+++ components/ResourcePage/selectionButton.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable chakra-ui/props-order */
-
 import React from 'react'
 import { Button, Icon, Text } from '@chakra-ui/react'
 import { IconType } from "react-icons";
@@ -15,26 +13,26 @@ interface SelectionIconProps {
 export default function SelectionIconButton({ text, icon, onClick, isActive }: SelectionIconProps = {}) {
   return (
     <Button
+      px={4}
+      py={2}
+      color="white"
+      bg="#111827"
+      borderRadius="md"
+      shadow="0 0 0 1px #3b82f6"
+      _hover={{ bg: "#1f2937" }}
+      _active={{ bg: "#374151" }}
       isActive = {isActive}
       leftIcon={
         <Icon
           as={icon}
-          color="black"
-          bg="#4ade80"
           w="30px"
           h="30px"
           p="4px"
+          color="black"
+          bg="#4ade80"
           borderRadius="md"
         />
       }
-      bg="#111827"
-      color="white"
-      borderRadius="md"
-      px={4}
-      py={2}
-      _hover={{ bg: "#1f2937" }}
-      _active={{ bg: "#374151" }}
-      shadow="0 0 0 1px #3b82f6"
       onClick={onClick}
     >
       <Text fontSize="md" fontWeight="medium">
